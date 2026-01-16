1fd2526fd271cd2995756febb16b2375627aec3e
Samuel Ibitowa <ibitowaifeoluwa@yahoo.com>
2026-01-16 15:17:28 +0100

    feat: add "provider" && "logger" to pub dependencies.

---
+++ lib/_core_/logger/logger.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:logger/logger.dart';
+
+final logger = Logger(
+  printer: PrettyPrinter(dateTimeFormat: DateTimeFormat.dateAndTime),
+);
