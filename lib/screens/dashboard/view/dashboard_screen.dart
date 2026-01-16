7707ec6aa185e0e5006880c4d1bffde5d33fda07
Samuel Ibitowa <ibitowaifeoluwa@yahoo.com>
2026-01-16 13:22:30 +0100

    feat: add dashboard view model.

--- lib/screens/dashboard/view/dashboard_screen.dart
+++ lib/screens/dashboard/view/dashboard_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../services/navigation_service/navigation_service.dart';
+import '../view_model/dashboard_view_model.dart';
 import '../widgets/dashboard_item_card.dart';
 import '../widgets/profile_card.dart';
 
@@ -10,85 +10,69 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFF5F7FA),
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        title: const Text(
-          'Dashboard',
-          style: TextStyle(color: Colors.black87, fontWeight: FontWeight.w600),
-        ),
-        centerTitle: false,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout, color: Colors.black87),
-            onPressed: () {
-              logOut(context);
-            },
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 8.h),
-
-            /// Profile Summary Card
-            ProfileCard(),
-
-            SizedBox(height: 24.h),
-
-            /// Section Title
-            Text(
-              'Recent Items',
-              style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.bold),
-            ),
-
-            SizedBox(height: 12.h),
-
-            /// Items List
-            Expanded(
-              child: ListView.separated(
-                itemCount: 6,
-                separatorBuilder: (_, _) => SizedBox(height: 12.h),
-                itemBuilder: (context, index) {
-                  return const DashboardItemCard();
-                },
+    return ListenableBuilder(
+      listenable: dashboardViewModel,
+      builder: (context, child) {
+        return Scaffold(
+          backgroundColor: const Color(0xFFF5F7FA),
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            title: const Text(
+              'Dashboard',
+              style: TextStyle(
+                color: Colors.black87,
+                fontWeight: FontWeight.w600,
               ),
             ),
-          ],
-        ),
-      ),
-    );
-  }
+            centerTitle: false,
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.logout, color: Colors.black87),
+                onPressed: () {
+                  dashboardViewModel.logout(context);
+                },
+              ),
+            ],
+          ),
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 8.h),
 
-  void logOut(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (_) => AlertDialog(
-        backgroundColor: Colors.white,
-        title: const Text('Log out'),
-        content: const Text('Are you sure you want to log out?'),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text('Cancel'),
+                /// Profile Summary Card
+                ProfileCard(),
+
+                SizedBox(height: 24.h),
+
+                /// Section Title
+                Text(
+                  'Recent Items',
+                  style: TextStyle(
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                SizedBox(height: 12.h),
+
+                /// Items List
+                Expanded(
+                  child: ListView.separated(
+                    itemCount: 6,
+                    separatorBuilder: (_, _) => SizedBox(height: 12.h),
+                    itemBuilder: (context, index) {
+                      return const DashboardItemCard();
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-              navigationService.navigateToAndRemoveAll(
-                destinationScreen: AppRoutes.loginScreen,
-                direction: .left,
-              );
-            },
-            child: const Text('Log out'),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
---
+++ lib/screens/dashboard/view_model/dashboard_view_model.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../../services/navigation_service/navigation_service.dart';
+
+class DashboardViewModel extends ChangeNotifier {
+  DashboardViewModel() {
+    init();
+  }
+
+  // ----------------------------
+  // Lifecycle
+  // ----------------------------
+  Future<void> init() async {}
+
+  // ----------------------------
+  // Actions
+  // ----------------------------
+  void logout(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        backgroundColor: Colors.white,
+        title: const Text('Log out'),
+        content: const Text('Are you sure you want to log out?'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              navigationService.navigateToAndRemoveAll(
+                destinationScreen: AppRoutes.loginScreen,
+                direction: .left,
+              );
+            },
+            child: const Text('Log out'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+final dashboardViewModel = DashboardViewModel();
